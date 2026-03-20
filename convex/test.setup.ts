baa198bea14bd2af8a4f6f64f6797e55a90836e8
Abel de Bruijn <abeldebruijn@hotmail.com>
2026-03-20 13:56:28 +0100

    fix(test): restrict test file glob to/js exclude d.ts

--- convex/test.setup.ts
+++ convex/test.setup.ts
@@ -4,7 +4,13 @@ import { convexTest } from "convex-test";
 
 import schema from "./schema";
 
-export const convexModules = import.meta.glob("./**/!(*.*.*)*.*s");
+export const convexModules = import.meta.glob([
+  "./**/*.ts",
+  "./**/*.js",
+  "!./**/*.test.ts",
+  "!./**/*.test.js",
+  "!./**/*.d.ts",
+]);
 
 export function createConvexTest() {
   return convexTest(schema, convexModules);
